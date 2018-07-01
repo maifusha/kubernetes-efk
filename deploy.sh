01bd77a8e60f8d4e2627dde9d6681ccc622ba7f9
lixin <1045909037@qq.com>
2018-07-02 02:47:02 +0800

    引入curator组件
    Signed-off-by: lixin <1045909037@qq.com>

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,12 @@
 
 NAMESPACE="efk"
 
-sed -i "s/\$NAMESPACE/$NAMESPACE/g" manifests/kibana/configmap.yaml
+sed -i "s/\$NAMESPACE/$NAMESPACE/g" manifests/kibana/deployment.yaml
 sed -i "s/\$NAMESPACE/$NAMESPACE/g" manifests/fluentd/daemonset.yaml
 sed -i "s/\$NAMESPACE/$NAMESPACE/g" manifests/fluentd/1-rbac-sa-psp.yaml
-sed -i "s/\$NAMESPACE/$NAMESPACE/g" manifests/elasticsearch/configmap.yaml
+sed -i "s/\$NAMESPACE/$NAMESPACE/g" manifests/elasticsearch/deployment.yaml
 sed -i "s/\$NAMESPACE/$NAMESPACE/g" manifests/elasticsearch/1-rbac-sa-psp.yaml
+sed -i "s/\$NAMESPACE/$NAMESPACE/g" manifests/elasticsearch/curator/cronjob.yaml
 sed -i "s/\$NAMESPACE/$NAMESPACE/g" manifests/elasticsearch/elastalert/configmap-main.yaml
 
 ./build.sh
